9ef7a78ae1dc16ec7e49ecae7f380c28103d834c
Lívia Amaral <125597286+Liviaamaralsales@users.noreply.github.com>
2024-08-06 18:24:08 -0300

    Formulario v2

--- form/node.js
+++ form/node.js
@@ -1,16 +1,40 @@
 $(document).ready(function(){
     alert("Seu site carregou!");
 
-    $('#btnCadastro').click(function(){
+        $("#btnCadastro").on("click", function(){
 
-        var inputNome = $("#txtNome").val();
-        var inputSobrenome = $("#txtSobrenome").val();
-        var inputIdade = $("#txtIdade").val();
-        var inputCPF = $("#txtCPF").val();
-        var inputTelefone = $("#txtTelefone").val();
-        var inputEmail = $("#txtEmail").val();
-        var inputCidade = $("#txtCidade").val();
+            var inputNome = $("#txtNome").val();
+            var inputSobrenome = $("#txtSobrenome").val();
+            var inputIdade = $("#txtIdade").val();
+            var inputCPF = $("#txtCPF").val();
+            var inputTelefone = $("#txtTelefone").val();
+            var inputEmail = $("#txtEmail").val();
+            var inputCidade = $("#cmbCidade").val();
 
-        alert("Cadastro realizado com as seguintes informações: \n Nome: " + inputNome + "\n Sobrenome: " + inputSobrenome + "\n Idade: " + inputIdade + "\n CPF: " + inputCPF + "\n Telefone: " +  inputTelefone + "\n Email: " + inputEmail + "\n Cidade: " + inputCidade)
-    })
+            if(inputNome == ""){
+                alert("Preencha seu nome!");
+                return 0;
+            } else if(inputSobrenome == ""){
+                alert("Preencha seu sobrenome!");
+                return 0;
+            } else if(inputIdade == ""){
+                alert("Preencha sua idade!");
+                return 0;
+            } else if(inputCPF == ""){
+                alert("Preencha seu CPF!");
+                return 0;
+            } else if(inputTelefone == ""){
+                alert("Preencha seu telefone!");
+                return 0;
+            } else if(inputEmail == ""){
+                alert("Preencha seu email!");
+                return 0;
+            } else if(inputCidade == ""){
+                alert("Selecione sua cidade!");
+                return 0;
+            } else {
+                alert("Cadastro realizado com as seguintes informações: \n Nome: " + inputNome + "\n Sobrenome: " + inputSobrenome + "\n Idade: " + inputIdade + "\n CPF: " + inputCPF + "\n Telefone: " +  inputTelefone + "\n Email: " + inputEmail + "\n Cidade: " + inputCidade)
+            }
+        })
+
 })
